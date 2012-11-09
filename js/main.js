7f1a018e961d32617d8b652173d3cb43c76d2081
Matt Ballert <nerdalert1984@fullsail.edu>
2012-11-08 20:17:37 -0500

    Updated project files to reflected week 3 changes.

--- js/main.js
+++ js/main.js
@@ -62,8 +62,14 @@ window.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 	
-	function storeData(){
+	function storeData(key){
+		if(!key){
 		var id					= Math.floor(Math.random()*100000001);
+		}else{
+			id = key;
+		}
+		getSelectedRadio();
+		getCheckboxValue();
 		var item				= {};
 			item.yourname		= ["Your Name", $('yourname').value];
 			item.email			= ["Email Adress", $('email').value];
@@ -93,6 +99,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		$('items').style.display = "block";
 		for(var i=0, j=localStorage.length; i<j; i++){
 			var makeli = document.createElement('li');
+			var linksLi = document.createElement('li');
 			makeList.appendChild(makeli);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
@@ -104,7 +111,73 @@ window.addEventListener("DOMContentLoaded", function () {
 				makeSubList.appendChild(makeSubli);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubli.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i), linksLi);
+		}
+	}
+	
+	function makeItemLinks(key, linksLi){
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Information";
+		editLink.addEventListener("click", editItem);
+		aditLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		var breakTag = document.createElement('br')
+		linksLi.appendChild(breakTag);
+		
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Information";
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+	
+	function editItem(){
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		toggleControls("off");
+		
+		$('yourname').value = item.yourname[1];
+		$('email').value = item.email[1]
+		$('comname').value = item.comname[1]
+		$('comsite').value = item.comsite[1]
+		$('gname').value = item.gname[1]
+		$('clink').value = item.clink[1]
+		$('estdate').value = item.estdate[1]
+		$('howhear').value = item.howhear[1]
+		var radios = document.forms[0].firstgame;
+		for (var i=0; i<radios.length; i++){
+			if(radios[i].value == "Yes" && item.firstgame[1] == "Yes"){
+			}else if(radios[i].value == "No" && item.firstgame[1] == "No"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		if(item.platform[1] == "table"){
+			$('platform').setAttribute("checked", "checked");
+		}
+		
+		submitLink.removeEventListener("click", validate);
+		$('submit').value = "Edit Information";
+		var editSubmit = $('submit')
+		editSubmit.addEventListener("click", validate);		
+		editSubmit.key = this.key;
+	}
+	
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete this information?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("Information deleted.");
+			window.location.reload();
+		}else{
+			alert("Information was NOT deleted.")
 		}
 	}
 	
@@ -118,16 +191,80 @@ window.addEventListener("DOMContentLoaded", function () {
 			return false;
 		}
 	}
+	
+	function validate(e){
+		var getYourname = $('yourname');
+		var getEmail = $('email');
+		var getComname = $('comname');
+		var getComsite = $('comsite');
+		var getGname = $('gname');
+		
+		errMsg.innerHTML = "";
+		getYourname.style.border = "1px solid black";
+		getEmail.style.border = "1px solid black";
+		getComname.style.border = "1px solid black";
+		getComsite.style.border = "1px solid black";
+		getGname.style.border = "1px solid black";
+		
+		var messageAry = [];
+		
+		if(getYourname.value === ""){
+			var nameError = "Please fill in your name.";
+			getYourname.style.border = "1px solid orange";
+			messageAry.push(nameError);
+		}
+		
+		
+		var emailRe = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(!(emailRe.exec(getEmail.value))){
+			var emailError = "Please fill in your e-mail address.";
+			getEmail.style.border = "1px solid orange";
+			messageAry.push(emailError);
+		}
+		
+		if(getComname.value === ""){
+			var comnameError = "Please fill in the name of your company.";
+			getComname.style.border = "1px solid orange";
+			messageAry.push(comnameError);
+		}
+		
+		if(getComsite.value === ""){
+			var siteError = "Please fill in web address of your company's website.";
+			getComsite.style.border = "1px solid orange";
+			messageAry.push(siteError);
+		}
+		
+		if(getGname.value === ""){
+			var gNameError = "Please fill in the name of your game.";
+			getGname.style.border = "1px solid orange";
+			messageAry.push(gNameError);
+		}
+		
+		if(messageAry.length >= 1){
+			for(var i=0, j=messageAry.length; i < j; i++){
+			var txt = document.createElement('li');
+			txt.innerHTML = messageAry[i];
+			errMsg.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;
+		}else{
+		storeData(this.key);
+		}
+	}
+		
 	// Variable Defaults
 	// var howHeard = ["--How Did You Hear About Us?--", "Facebook", "Twitter", "Google+", "Other" ];
 //	howHear();
-
+	
 	var firstValue
 	var	platValue
+	var errMsg = $('errors');
 	//Set Links & Submit
 	var submitLink = $("submit")
-	submitLink.addEventListener("click", storeData);
+	submitLink.addEventListener("click", validate);
 	var clearLink = $("clear");
 	clearLink.addEventListener("click", clearLocal);
 	var displayLink = $("display")
 	displayLink.addEventListener("click", getData)});
+	
